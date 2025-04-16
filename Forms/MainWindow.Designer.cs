9c398a3e11c9bcf2fe61ed6026d3423cb3bcad9a
Viktor Lychkatyi <137647165+ViktorLychkatyi@users.noreply.github.com>
2025-04-16 16:06:28 +0200

    File Search

---
+++ Forms/MainWindow.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace FileSearch
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listView1 = new ListView();
+            columnHeader1 = new ColumnHeader();
+            columnHeader2 = new ColumnHeader();
+            columnHeader3 = new ColumnHeader();
+            columnHeader4 = new ColumnHeader();
+            columnHeader5 = new ColumnHeader();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // listView1
+            // 
+            listView1.BackColor = SystemColors.Window;
+            listView1.Columns.AddRange(new ColumnHeader[] { columnHeader1, columnHeader2, columnHeader3, columnHeader4, columnHeader5 });
+            listView1.GridLines = true;
+            listView1.HoverSelection = true;
+            listView1.ImeMode = ImeMode.Alpha;
+            listView1.Location = new Point(12, 46);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(965, 247);
+            listView1.TabIndex = 3;
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.View = View.Details;
+            listView1.SelectedIndexChanged += listView1_SelectedIndexChanged;
+            // 
+            // columnHeader1
+            // 
+            columnHeader1.Text = "Имя";
+            columnHeader1.Width = 300;
+            // 
+            // columnHeader2
+            // 
+            columnHeader2.Text = "Путь";
+            columnHeader2.Width = 350;
+            // 
+            // columnHeader3
+            // 
+            columnHeader3.Text = "Размер";
+            columnHeader3.Width = 110;
+            // 
+            // columnHeader4
+            // 
+            columnHeader4.Text = "Изменен";
+            columnHeader4.Width = 120;
+            // 
+            // columnHeader5
+            // 
+            columnHeader5.Text = "Тип";
+            columnHeader5.Width = 64;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 12);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "Введите";
+            textBox1.Size = new Size(869, 23);
+            textBox1.TabIndex = 4;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(887, 11);
+            button1.Name = "button1";
+            button1.Size = new Size(90, 23);
+            button1.TabIndex = 5;
+            button1.Text = "Поиск";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click_2;
+            // 
+            // MainWindow
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(989, 305);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(listView1);
+            Name = "MainWindow";
+            Text = "Поиск файлов";
+            Load += MainWindow_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        #endregion
+
+        private ListView listView1;
+        private ColumnHeader columnHeader1;
+        private ColumnHeader columnHeader2;
+        private ColumnHeader columnHeader3;
+        private ColumnHeader columnHeader4;
+        private TextBox textBox1;
+        private ColumnHeader columnHeader5;
+        private Button button1;
+    }
+}
---
+++ Forms/MainWindow.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.Design;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace FileSearch
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ShowUI();
+            ShowFiles();
+            ShowDirectories();
+        }
+
+        private void ShowFiles()
+        {
+            Thread loadFiles = new Thread(LoadFiles);
+            //loadFiles.Start();
+            loadFiles.IsBackground = true;
+        }
+
+        private void ShowDirectories()
+        {
+            Thread showDirectory = new Thread(ShowDirectory);
+            showDirectory.Start();
+            showDirectory.IsBackground = true;
+        }
+
+        private void ShowUI()
+        {
+            listView1.View = View.Details;
+            listView1.FullRowSelect = true;
+            listView1.HoverSelection = true;
+            listView1.Cursor = Cursors.Hand;
+        }
+
+        private void LoadFiles()
+        {
+            List<DriveInfo> drives = DriveInfo.GetDrives().ToList();
+            List<DirectoryInfo> directories = new List<DirectoryInfo>();
+            List<DirectoryInfo> subDirectories = new List<DirectoryInfo>();
+            List<FileInfo> files = new List<FileInfo>();
+
+            string[] sysPaths = { "Config.Msi" };
+
+            try
+            {
+                foreach (DriveInfo drive in drives)
+                {
+                    foreach (DirectoryInfo directoryInfo in drive.RootDirectory.GetDirectories())
+                    {
+                        if (directoryInfo.Attributes.HasFlag(FileAttributes.System) && !sysPaths.Contains(directoryInfo.Name))
+                        {
+                            continue;
+                        }
+                        if (sysPaths.Contains(directoryInfo.Name))
+                        {
+                            continue;
+                        }
+
+                        foreach (FileInfo file in directoryInfo.GetFiles())
+                        {
+                            if (file.Attributes.HasFlag(FileAttributes.System) && !sysPaths.Contains(file.Name))
+                            {
+                                continue;
+                            }
+                            if (sysPaths.Contains(file.Name))
+                            {
+                                continue;
+                            }
+                            ListViewItem item = new ListViewItem(file.Name);
+                            item.SubItems.Add(file.FullName);
+                            item.SubItems.Add((file.Length / 1024) + " KB");
+                            item.SubItems.Add(file.LastWriteTime.ToString());
+                            item.SubItems.Add(file.Extension);
+                            listView1.Items.Add(item);
+                        }
+                    }
+                }
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}");
+            }
+        }
+
+        private void ShowDirectory()
+        {
+            textBox1.AutoCompleteMode = AutoCompleteMode.SuggestAppend;
+            textBox1.AutoCompleteSource = AutoCompleteSource.CustomSource;
+            AutoCompleteStringCollection autoComplete = new AutoCompleteStringCollection();
+            textBox1.AutoCompleteCustomSource = autoComplete;
+
+            List<DriveInfo> drives = DriveInfo.GetDrives().ToList();
+            List<DirectoryInfo> directories = new List<DirectoryInfo>();
+            List<DirectoryInfo> subDirectories = new List<DirectoryInfo>();
+            List<FileInfo> files = new List<FileInfo>();
+
+            string[] sysPaths = { "Config.Msi" };
+
+            try
+            {
+                foreach (DriveInfo drive in drives)
+                {
+                    foreach (DirectoryInfo directoryInfo in drive.RootDirectory.GetDirectories())
+                    {
+                        if (directoryInfo.Attributes.HasFlag(FileAttributes.System) && !sysPaths.Contains(directoryInfo.Name))
+                        {
+                            continue;
+                        }
+                        if (sysPaths.Contains(directoryInfo.Name))
+                        {
+                            continue;
+                        }
+                        directories.Add(directoryInfo);
+                        autoComplete.Add(directoryInfo.FullName);
+                    }
+                }
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}");
+            }
+        }
+
+        public void MainWindow_Load()
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void StartSearch()
+        {
+            string path = textBox1.Text;
+
+            if (!Directory.Exists(path))
+            {
+                MessageBox.Show("Указанная директория не существует.");
+                return;
+            }
+
+            listView1.Items.Clear();
+
+            List<DriveInfo> drives = DriveInfo.GetDrives().ToList();
+            List<DirectoryInfo> directories = new List<DirectoryInfo>();
+            List<DirectoryInfo> subDirectories = new List<DirectoryInfo>();
+            List<FileInfo> files = new List<FileInfo>();
+
+            string[] sysPaths = { "Config.Msi" };
+
+            try
+            {
+                foreach (DriveInfo drive in drives)
+                {
+                    foreach (DirectoryInfo directoryInfo in drive.RootDirectory.GetDirectories())
+                    {
+                        if (directoryInfo.Attributes.HasFlag(FileAttributes.System) && !sysPaths.Contains(directoryInfo.Name))
+                        {
+                            continue;
+                        }
+                        if (sysPaths.Contains(directoryInfo.Name))
+                        {
+                            continue;
+                        }
+                        directories.Add(directoryInfo);
+                    }
+                }
+
+                foreach (DirectoryInfo directory in directories)
+                {
+                    if (directory.FullName == path)
+                    {
+                        subDirectories.Add(directory);
+                    }
+                }
+
+                foreach (DirectoryInfo directory in subDirectories)
+                {
+                    foreach (FileInfo file in directory.GetFiles())
+                    {
+                        if (file.Attributes.HasFlag(FileAttributes.System) && !sysPaths.Contains(file.Name))
+                        {
+                            continue;
+                        }
+                        if (sysPaths.Contains(file.Name))
+                        {
+                            continue;
+                        }
+                        ListViewItem item = new ListViewItem(file.Name);
+                        item.SubItems.Add(file.FullName);
+                        item.SubItems.Add((file.Length / 1024) + " KB");
+                        item.SubItems.Add(file.LastWriteTime.ToString());
+                        item.SubItems.Add(file.Extension);
+                        listView1.Items.Add(item);
+                    }
+                }
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}");
+            }
+            MessageBox.Show("Поиск успешно завершен.");
+        }
+
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+            StartSearch();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBox1.Multiline = false;
+        }
+    }
+}
